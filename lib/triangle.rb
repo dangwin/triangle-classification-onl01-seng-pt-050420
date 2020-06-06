58c075115f2729b4d9233c58b7fd43a0f98abbbf
Dang Nguyen <danghwin@gmail.com>
2020-06-06 17:11:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,11 @@
 class Triangle
-  # write code here
-  
-   def initialize(side_1, side_2, side_3)
+  def initialize(side_1, side_2, side_3)
     @triangle_sides = []
     @triangle_sides << side_1
     @triangle_sides << side_2
     @triangle_sides << side_3
   end
-  
+
   def valid?
     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
     sum_one_three = @triangle_sides[0] + @triangle_sides[2]
@@ -20,12 +18,12 @@ class Triangle
       return false
     end
   end
-  
-  def kind 
+
+  def kind
     if valid?
-      if triangle_sides.uniq.length == 1 
-        return :equilateral 
-         elsif @triangle_sides.uniq.length == 2
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
         return :isosceles
       else
         return :scalene
@@ -37,6 +35,5 @@ class Triangle
 end
 
 class TriangleError < StandardError
-
 
 end
