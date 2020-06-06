c77817039cdf5a3f22f35eb9f46152a77c07a443
Dang Nguyen <danghwin@gmail.com>
2020-06-06 17:19:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,39 +1,33 @@
+
+
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
-  end
 
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+  attr_accessor :a, :b, :c
 
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    elsif
+    sides = [a,b,c]
+    ineq = [a+b > c, a+c > b, b+c > a]
+
+    if sides.min <= 0 || ineq.include?(false)
       raise TriangleError
     end
+
+    if a == b && b == c
+      :equilateral
+    elsif a != b && a != c && b != c
+      :scalene
+    elsif (a == b && b != c) || (b == c && c != a) || (a == c && c != b)
+      :isosceles
+    end
   end
+  
+  class TriangleError < StandardError
 end
-
-class TriangleError < StandardError
-
+  
 end
